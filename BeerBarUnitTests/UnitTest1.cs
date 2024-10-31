1f5c30a9ab82c297d50649ff99b0a60f03feaaf3
Nikita Sparham <nikita_init@hotmail.com>
2024-10-31 13:45:17 +0100

    made unittests

--- BeerBarUnitTests/UnitTest1.cs
+++ BeerBarUnitTests/UnitTest1.cs
@@ -3,10 +3,27 @@ namespace BeerBarUnitTests
     [TestClass]
     public class UnitTest1
     {
+
+
         [TestMethod]
         public void TestMethod1()
         {
 
         }
     }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+}
+
+[TestMethod]
+public void TestMethod1()
+{
+
+}
+    }
+}
 }
